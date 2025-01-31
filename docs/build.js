05419da14eb95101f3139fdd8175bcdbe29d22b1
AInovix <AInovix@proton.me>
2025-02-01 03:47:43 +1000

    Create build.js

---
+++ docs/build.js
@@ -0,0 +1,15 @@
+const fs = require('fs');
+const { Converter } = require('showdown');
+const prism = require('prismjs');
+
+// Copy needed prism components
+require('prismjs/components/prism-javascript');
+require('prismjs/components/prism-python');
+
+// Build function
+function buildDocs() {
+  // Add your build logic here
+  console.log('Docs built successfully');
+}
+
+buildDocs();
